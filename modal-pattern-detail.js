df0448717ab5e8b6894331b2d995005ec44f2d76
Akvikk <ashwinikusahu2001@gmail.com>
2026-03-20 20:23:17 +0530

    refactor: relocate modal component files to `js/modules/ui/components` directory.

--- modal-pattern-detail.js
+++ modal-pattern-detail.js
@@ -1,10 +1,2 @@
-(function () {
-    const template = document.createElement('template');
-    template.innerHTML = `
-    <div id="patternDetailModal" class="fixed inset-0 z-[60] bg-black/60 backdrop-blur-md hidden flex items-center justify-center p-4" onclick="toggleModal('patternDetailModal')">
-        <div class="glass-menu modal-surface w-full max-w-xl flex flex-col overflow-hidden" onclick="event.stopPropagation()">
-            <div id="patternDetailBody"></div>
-        </div>
-    </div>`;
-    document.body.appendChild(template.content);
-})();
+// This file has been moved to js/modules/ui/components/modal-pattern-detail.js
+// You can safely delete this file.
